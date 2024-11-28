b6a054113f7afd20fe44d894072102eb81f55282
Viha <156207386+VihaShomikha@users.noreply.github.com>
2024-11-28 22:32:26 +0530

    Updated Type animation content

--- assets/type.js
+++ assets/type.js
@@ -1,7 +1,7 @@
 const typedTextSpan = document.querySelector(".typed-text");
 const cursorSpan = document.querySelector(".cursor");
 
-const textArray = ["UI/UX Design", "Web Development", "IOT"];
+const textArray = ["UI/UX Designing", "Web Development", "IOT"];
 const typingDelay = 200;
 const erasingDelay = 100;
 const newTextDelay = 2000; // Delay between current and next text
